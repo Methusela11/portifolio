d93fe87c519b0c20350127e9c4d548958ae93b33
Methusela11 <soenkilifi.com>
2026-03-27 20:47:14 +0300

    project template css

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     link = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self): return self.title
+    
 class ContactMessage(models.Model):
     name = models.CharField(max_length=120)
     email = models.EmailField()
