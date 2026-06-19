ec8a7d31af5d3fd76310fc5d848126784acb2c1b
Methusela11 <soenkilifi@gmail.com>
2026-06-20 00:41:41 +0300

    Update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,6 +9,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '.onrender.com',
+    '*',
 ]
 
 INSTALLED_APPS = ['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','main']
